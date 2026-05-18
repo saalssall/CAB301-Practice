7d59fc5237ccbb9d6d7cadc6a20c1a21f2691c6b
Hamidullah Rezae <hamidullahrezae15@gmail.com>
2026-05-19 08:44:43 +1000

    Add graph implementation

--- Graphs/Program.cs
+++ Graphs/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace Graphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var friends = new Dictionary<string, List<string>>
+            {
+                { "Alice",   new List<string> { "Bob", "Diana", "Fred" } },
+                { "Bob",     new List<string> { "Alice", "Cynthia", "Diana" } },
+                { "Cynthia", new List<string> { "Bob" } },
+                { "Diana",   new List<string> { "Alice", "Bob", "Fred" } },
+                { "Elise",   new List<string> { "Fred" } },
+                { "Fred",    new List<string> { "Alice", "Diana", "Elise" } }
+            };
+            
+            foreach (string friend in friends["Alice"])
+            {
+                Console.WriteLine(friend);
+            }
+        }
+    }
+}
